a585e6bfae39f8db1e7056bf99f99d788154b01f
rishiakkaladevi <rishiakkaladevi0@gmail.com>
2025-07-20 17:19:19 +0530

    intial commit

---
+++ youtubebot.py
@@ -0,0 +1,85 @@
+from youtube_transcript_api import YouTubeTranscriptApi , TranscriptsDisabled
+from langchain_core.runnables import RunnablePassthrough,RunnableParallel,RunnableLambda
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_groq import ChatGroq
+from langchain_huggingface import HuggingFaceEmbeddings
+from langchain_community.vectorstores import FAISS
+from dotenv import load_dotenv
+from langchain_huggingface import HuggingFaceEndpoint, ChatHuggingFace
+from langchain_core.prompts.prompt import PromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+import streamlit as st
+def mainfun(id,question):
+    try:
+        transcriptlist=YouTubeTranscriptApi.get_transcript(video_id=id,languages=['en'])
+        transcript = ' '.join(x['text'] for x in transcriptlist)
+        print(transcript)
+    except TranscriptsDisabled:
+        print("captions for this video are not available")
+
+
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size = 1000, chunk_overlap=50)
+    chunks = text_splitter.create_documents([transcript])
+
+
+    embedding_model = HuggingFaceEmbeddings(
+        model_name="all-MiniLM-L6-v2"
+    )
+
+    vectorstore = FAISS.from_documents(chunks,embedding_model)
+
+    #vectorstore.index_to_docstore_id
+
+    retriever = vectorstore.as_retriever(search_type="similarity",search_kwargs={'k':4})
+
+    load_dotenv()
+    llm=HuggingFaceEndpoint(
+        repo_id='moonshotai/Kimi-K2-Instruct',
+        task = 'text-generation'
+        )
+    model = ChatHuggingFace(llm=llm)
+
+    prompt= PromptTemplate(
+        template=''' You are a helpful assistant.
+                Answer ONLY from the provided transcript content.
+                If the context is insufficient,say that you don't know
+                {context}
+                question : {question}''',
+                input_variables=['context','question']
+                )
+
+    question = " "
+    retrieved_docs = retriever.invoke(question)
+
+    def contextret(retrieved_docs):
+        contextpara = '\n\n'.join(content.page_content for content in retrieved_docs)
+        return contextpara
+
+    #final_prompt=prompt.invoke({"context": contextret(retrieved_docs) ,"question":question})
+
+
+    parallelchain = RunnableParallel({
+        "context" : retriever | RunnableLambda(contextret),
+        "question" : RunnablePassthrough()
+    })
+
+    parser = StrOutputParser()
+
+    mainchain = parallelchain | prompt | model | parser
+
+
+
+
+    return mainchain.invoke(question)
+
+
+st.title("Youtube Video Bot")
+video_id= st.text_input(label="",placeholder="enter your youtube video id here ")
+if st.button("submit"):
+    st.session_state.submitted=True
+if st.session_state.get("submitted",False):
+    query=st.text_input(label="Ask me about the video")
+    if st.button("answer"):
+        st.text(mainfun(video_id,query))
+        
+
